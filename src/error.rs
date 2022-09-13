edf2dea72774251f82d8cfbec4bcc1f786a4503d
Kyle Harris <kyle@learnwithjuni.com>
2022-09-13 12:05:23 -0600

    impl processor for init escrow

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,8 @@ pub enum EscrowError {
     /// Invalid instruction
     #[error("Invalid Instruction")]
     InvalidInstruction,
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
 }
 
 impl From<EscrowError> for ProgramError {
